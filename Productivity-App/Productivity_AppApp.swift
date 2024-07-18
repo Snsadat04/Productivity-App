7de38754f04bdc884308622c122a543997e5d94c
Soyeb Sadat <Snsadat04@gmail.com>
2024-07-17 22:19:30 -0400

    Initial Commit

---
+++ Productivity-App/Productivity_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Productivity_AppApp.swift
+//  Productivity-App
+//
+//  Created by Soyeb Sadat on 7/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Productivity_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
